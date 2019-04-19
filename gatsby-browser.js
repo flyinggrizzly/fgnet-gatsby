7d0d576062e7fa089398f664c475f6f720e20d6c
Sean DMR <sean@flyinggrizzly.net>
2019-04-19 21:51:53 +0100

    Import styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import './src/styles/bootstrap.min.css'
+import './src/styles/sidebar.css'
+import './src/styles/main.css'
 import './src/styles/global.css'
